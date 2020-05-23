2457b8739efce15899d087d729e96861ce350e1a
CuteMustard <55762397+CuteMustard@users.noreply.github.com>
2020-05-23 16:23:55 -0500

    More plant card styling

--- frontend/components/Plant.js
+++ frontend/components/Plant.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Image } from "react-native";
-import { Text, Title, Card, CardItem } from "native-base";
+import { Text, Title, Card, CardItem, Body } from "native-base";
 import moment from "moment";
 
 export default function Plant(props) {
@@ -16,16 +16,20 @@ export default function Plant(props) {
         onPress={() => props.nav.navigate("PlantDetail")}
         style={{backgroundColor: '#B4CDD1'}}
       >
-        <Image
-          source={{
-            uri: props.plant.image,
-          }}
-          style={{ height: 100, width: 100, justifyContent: 'center',
-          alignItems: 'center', }}
-        />
+        <Body style={{justifyContent: 'center', alignItems: 'center', }}>
+          <Image
+            source={{
+              uri: props.plant.image,
+            }}
+            style={{ height: 100, width: 100, }}
+          />
+        </Body>
+        
       </CardItem>
       <CardItem style={{backgroundColor: '#B4CDD1'}}>
+        <Body style={{justifyContent: 'center', alignItems: 'center', }}>
         <Text style={{ textAlign: 'center', color: "#fff", textShadowColor: 'rgba(0, 0, 0, 0.3)', textShadowOffset: {width: 0, height: 1}, textShadowRadius: 5}}>{moment(props.plant.date_added).format("l")}</Text>
+        </Body>
       </CardItem>
     </Card>
   );
