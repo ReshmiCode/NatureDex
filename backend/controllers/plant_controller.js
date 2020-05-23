ef14450b331dbc7f7b95b8fd016f18c8ee46e749
SaloniSS <sxs170015@utdallas.edu>
2020-05-23 00:54:19 -0500

    DELETE plant works

--- backend/controllers/plant_controller.js
+++ backend/controllers/plant_controller.js
@@ -69,7 +69,29 @@ exports.getPlant = async (req, res, next) => {
 }
 
 exports.deletePlant = async (req, res, next) => {    
-    return "ToDo";
+    try {
+        const plant = await Plant.findById(req.params.id);
+
+        if(!plant){
+            return res.status(404).json({
+                success: false,
+                error: 'No plant found'
+            });
+        }
+
+        await plant.remove();
+
+        return res.status(200).json({
+            success: true,
+            data: plant
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
 }
 
 exports.getUserPlant = async (req, res, next) => {    
