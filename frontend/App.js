8ab3b0e068e2acf4885e1baecce9218de3283d06
SaloniSS <sxs170015@utdallas.edu>
2020-05-22 22:48:24 -0500

    Error fix

--- frontend/App.js
+++ frontend/App.js
@@ -17,8 +17,6 @@ export default function App(props) {
 
   const isLoadingComplete = useCachedResources();
 
-  const isLoadingComplete = useCachedResources();
-
   if (!isLoadingComplete) {
     return null;
   } else {
