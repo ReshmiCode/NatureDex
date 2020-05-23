e1c8ccc034c49866c141348675f658e31e098c48
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-05-22 20:38:06 -0500

    base for screen

--- frontend/screens/LogScreen.js
+++ frontend/screens/LogScreen.js
@@ -1,17 +1,38 @@
 import * as React from "react";
-import { Container, Body, Content, Header, Title, Text } from "native-base";
+import { Image } from "react-native";
+import {
+  Container,
+  Body,
+  Button,
+  Content,
+  Header,
+  Title,
+  Text,
+} from "native-base";
+import plants from "../assets/images/plantoutlines.png";
 
 export default function LogScreen() {
   return (
     <Container>
       <Header>
         <Body>
-          <Title>Log Activity</Title>
+          <Title>NatureDex</Title>
         </Body>
       </Header>
       <Content padder>
         <Body>
-          <Text>hi</Text>
+          <Title>Identify a plant by a picture!</Title>
+          <Text>Please make sure the plant is centered in the photo.</Text>
+          <Image
+            source={plants}
+            style={{ height: 200, width: null, flex: 1 }}
+          />
+          <Button info>
+            <Text> Take Photo </Text>
+          </Button>
+          <Button info>
+            <Text> Choose From Camera Roll </Text>
+          </Button>
         </Body>
       </Content>
     </Container>
