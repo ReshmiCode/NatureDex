b92b3a61611a040fa8ca36a621cb87ae49fac5a5
CuteMustard <55762397+CuteMustard@users.noreply.github.com>
2020-05-23 16:01:10 -0500

    A little bit of styling to profile page

--- frontend/components/Plant.js
+++ frontend/components/Plant.js
@@ -1,28 +1,31 @@
 import React from "react";
 import { Image } from "react-native";
-import { Text, Card, CardItem } from "native-base";
+import { Text, Title, Card, CardItem } from "native-base";
 import moment from "moment";
 
 export default function Plant(props) {
   return (
-    <Card>
-      <CardItem>
-        <Text>{props.plant.description[0]}</Text>
+    <Card style={{backgroundColor: '#B4CDD1'}}>
+      
+      <CardItem style={{backgroundColor: '#B4CDD1'}}>
+        <Title style={{color: "#fff", textShadowColor: 'rgba(0, 0, 0, 0.3)', textShadowOffset: {width: 0, height: 1}, textShadowRadius: 5}}>{props.plant.description[0]}</Title>
       </CardItem>
       <CardItem
         cardBody
         button
         onPress={() => props.nav.navigate("PlantDetail")}
+        style={{backgroundColor: '#B4CDD1'}}
       >
         <Image
           source={{
             uri: props.plant.image,
           }}
-          style={{ height: 100, width: 100 }}
+          style={{ height: 100, width: 100, justifyContent: 'center',
+          alignItems: 'center', }}
         />
       </CardItem>
-      <CardItem>
-        <Text>{moment(props.plant.date_added).format("l")}</Text>
+      <CardItem style={{backgroundColor: '#B4CDD1'}}>
+        <Text style={{ textAlign: 'center', color: "#fff", textShadowColor: 'rgba(0, 0, 0, 0.3)', textShadowOffset: {width: 0, height: 1}, textShadowRadius: 5}}>{moment(props.plant.date_added).format("l")}</Text>
       </CardItem>
     </Card>
   );
