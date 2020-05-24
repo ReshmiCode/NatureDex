242a3c9b8266b1fc46973731b4c896f93de58b2d
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-05-23 23:58:00 -0500

    add context

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import * as React from "react";
+import React from "react";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
 
 import useCachedResources from "./hooks/useCachedResources";
@@ -9,6 +9,7 @@ import LoginScreen from "./screens/LoginScreen";
 import { StyleProvider } from "native-base";
 import getTheme from "./native-base-theme/components";
 import commonColor from "./native-base-theme/variables/commonColor";
+import { SettingsContext } from "./components/SettingsContext";
 
 //import { Apploading, Font } from "expo";
 //import PlayfairDisplay from "./assets/fonts/PlayfairDisplay.ttf";
@@ -25,34 +26,36 @@ export default function App(props) {
   } else {
     return (
       <StyleProvider style={getTheme(commonColor)}>
-        <View style={styles.container}>
-          {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
-          <NavigationContainer>
-            <Stack.Navigator>
-              <Stack.Screen
-                name="LogIn"
-                options={{ headerShown: false }}
-                component={LoginScreen}
-              />
-              <Stack.Screen
-                name="Root"
-                component={BottomTabNavigator}
-                options={{
-                  title: "NatureDex",
-                  headerTitleAlign: "center",
-                  headerLeft: null,
-                  headerTitleStyle: {
-                    fontSize: 32,
-                    color: "#4A5754",
-                  },
-                  headerStyle: {
-                    backgroundColor: "#B4CDD1",
-                  },
-                }}
-              />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </View>
+        <SettingsContext.Provider>
+          <View style={styles.container}>
+            {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
+            <NavigationContainer>
+              <Stack.Navigator>
+                <Stack.Screen
+                  name="LogIn"
+                  options={{ headerShown: false }}
+                  component={LoginScreen}
+                />
+                <Stack.Screen
+                  name="Root"
+                  component={BottomTabNavigator}
+                  options={{
+                    title: "NatureDex",
+                    headerTitleAlign: "center",
+                    headerLeft: null,
+                    headerTitleStyle: {
+                      fontSize: 32,
+                      color: "#4A5754",
+                    },
+                    headerStyle: {
+                      backgroundColor: "#B4CDD1",
+                    },
+                  }}
+                />
+              </Stack.Navigator>
+            </NavigationContainer>
+          </View>
+        </SettingsContext.Provider>
       </StyleProvider>
     );
   }
---
+++ frontend/components/SettingsContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+export const SettingsContext = createContext({
+  pokemonMode: false,
+  optionsMode: false,
+});
