0d170e772e125603c6f9b16c3da64d84ee203f54
SaloniSS <sxs170015@utdallas.edu>
2020-05-24 02:23:59 -0500

    get Plants

--- website/src/components/UserPlants.js
+++ website/src/components/UserPlants.js
@@ -1,13 +1,24 @@
-import React, {useState} from "react";
+import React, {useState , useEffect} from "react";
 import "../App.css";
+const axios = require("axios").default;
 
 function UserPlants(props) {
     let [plants, setPlants] = useState([]);
 
+      useEffect(() => {
+        async function fetchData() {
+            let url = 'https://backyardhacks2020.wl.r.appspot.com/api/v1/plants/user/' + props.user
+            const response = await axios(url);
+            console.log(response);
+            setPlants(response.data.data);
+        }
+        fetchData();
+      }, []);
+
     return (
         <div>
             {
-                console.log("Google ID", props.user)
+                console.log(plants)
             }
             {plants.length == 0 ? (
                 <p>You don't have any plants, get looking!</p>
